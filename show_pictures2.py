6d8684e10482c7dc92bf47774f7b0de900f21e50
Chad Burkins <chad.burkins@gmail.com>
2016-04-18 00:51:05 +0000

    Testing new jQuery Cycle2

--- show_pictures2.py
+++ show_pictures2.py
@@ -195,7 +195,7 @@ print """
 .lower-caption { width: 80%; margin:auto; border:3px solid #bbb; background: #eee }
 .caption { font-size: 100%; }
 .caption-category { font-weight: bold; color: red; }
-.cycle-overlay { font-family: tahoma, arial; position: absolute; bottom: 0; width: 70%; z-index: 600; background: black; color: white; padding: 15px; opacity: .3;}
+.cycle-overlay { font-family: tahoma, arial; margin:auto; position: absolute; bottom: 0; width: 70%; z-index: 600; background: black; color: white; padding: 15px; opacity: .3;}
 </style>
 """
 
