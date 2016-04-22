28900dc50f68c98aaac98b8ca2eb6d97467d3315
Chad Burkins <chad.burkins@gmail.com>
2016-04-22 10:24:48 +0000

    Testing new jQuery Cycle2

--- FS_common.py
+++ FS_common.py
@@ -6,7 +6,6 @@
 #   - show_pictures.py
 #  
   
-SRC_PATH = '/mnt/ChadDocs/My Webs/Web Albums/Web Albums/Family Pics Production - Turtle/'
 SRC_PATH = '/mnt/ChadDocs/My Webs/www.burkins.com/01 - Web Albums/Family Pics - Turtle - Production/'
 DST_URL = 'http://www.burkins.com/family/pictures/'
 Root = '/home3/cburkins/public_html/family/pictures/album/'
