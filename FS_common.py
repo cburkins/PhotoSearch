c274a70ce292534ffe31acd88ef8e9aa0e2f2095
Chad Burkins <chad.burkins@gmail.com>
2016-04-22 10:29:21 +0000

    Testing new jQuery Cycle2

--- FS_common.py
+++ FS_common.py
@@ -6,11 +6,19 @@
 #   - show_pictures.py
 #  
   
+# Path for each picture on my local Linux machien at home
 SRC_PATH = '/mnt/ChadDocs/My Webs/www.burkins.com/01 - Web Albums/Family Pics - Turtle - Production/'
+# Path where same picture is placed on the webserver
 DST_URL = 'http://www.burkins.com/family/pictures/'
+
+# The local directory on webserver
 Root = '/home3/cburkins/public_html/family/pictures/album/'
 Image_Metadata = Root + '/Image_Metadatav2'
 KM_Keyword_Cache = Root + '/.KM_keyword_cache'
+
+Root = '/home3/cburkins/public_html/family/pictures'
+Image_Metadata = Root + '/album/Image_Metadatav2'
+KM_Keyword_Cache = Root + '/album/.KM_keyword_cache'
 
 # --------------------------------------------------------------------------
 
