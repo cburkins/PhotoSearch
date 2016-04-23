52c3d72d98c13149b202dc3c3e2ddf0fc164de52
Chad Burkins <chad.burkins@gmail.com>
2016-04-23 19:17:18 +0000

    Testing new jQuery Cycle2

--- caption.py
+++ caption.py
@@ -6,21 +6,10 @@ import cgi
 
 ROOT = "/home3/cburkins/public_html/family/pictures/search/iptcinfo"
 sys.path.insert(0, ROOT)
-from iptcinfo import IPTCInfo
+
 import sys
 
-fn = (len(sys.argv) > 1 and [sys.argv[1]] or ['/home3/cburkins/test.jpg'])[0]
-
-# Create new info object
-info = IPTCInfo(fn)
-
-# Check if file had IPTC data
-if len(info.data) < 4: raise Exception(info.error)
-
-# Print list of keywords, supplemental categories, contacts
-print info.keywords
-print info.supplementalCategories
-print info.contacts
+from PIL import Image, ExifTags
 
 # ----------------------------------------------------- End ------------------------------------------
 
