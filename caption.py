f0164b8288a6ae27b2340b0221d2b8b7e7f2a607
Chad Burkins <chad.burkins@gmail.com>
2016-04-23 19:09:19 +0000

    Testing new jQuery Cycle2

--- caption.py
+++ caption.py
@@ -4,12 +4,23 @@ import os
 import sys
 import cgi
 
-ROOT = "/home3/cburkins/public_html/family/pictures/search/exif-py"
+ROOT = "/home3/cburkins/public_html/family/pictures/search/iptcinfo"
 sys.path.insert(0, ROOT)
+from iptcinfo import IPTCInfo
+import sys
 
-# Import my local code
-from EXIF import exifread
+fn = (len(sys.argv) > 1 and [sys.argv[1]] or ['~/test.jpg'])[0]
 
+# Create new info object
+info = IPTCInfo(fn)
+
+# Check if file had IPTC data
+if len(info.data) < 4: raise Exception(info.error)
+
+# Print list of keywords, supplemental categories, contacts
+print info.keywords
+print info.supplementalCategories
+print info.contacts
 
 # ----------------------------------------------------- End ------------------------------------------
 
