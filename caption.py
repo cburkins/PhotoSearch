451e27c83c006be2530b754dd5ac9f941c5dd412
Chad Burkins <chad.burkins@gmail.com>
2016-04-23 18:50:11 +0000

    Testing new jQuery Cycle2

--- caption.py
+++ caption.py
@@ -4,7 +4,7 @@ import os
 import sys
 import cgi
 
-ROOT = "/home3/cburkins/public_html/family/pictures/search"
+ROOT = "/home3/cburkins/public_html/family/pictures/search/exif-py"
 sys.path.insert(0, ROOT)
 
 # Import my local code
