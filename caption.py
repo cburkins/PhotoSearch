811750b1fe8fc94902ab2a50572bc7d59b63f511
Chad Burkins <chad.burkins@gmail.com>
2016-04-23 01:37:13 +0000

    Testing new jQuery Cycle2

---
+++ caption.py
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Image::ExifTool qw(ImageInfo);
+
+my $filename = "filename"
+
+# Load EXIF data for new Picture
+my $info = $exifTool->ImageInfo($filename);
+
+# Get the caption from the picture
+$Picture_Caption = $exifTool->GetValue('Caption-Abstract');
+if (! defined $Picture_Caption) { $Picture_Caption = ""; };
+
